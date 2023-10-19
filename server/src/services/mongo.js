acc44c3590da80ad9091f6c26f9ce6019577285d
Nisarg221B <nisarg.b.patel59@gmail.com>
2023-10-19 10:18:39 +0530

    feat: add mongoDB for github actions

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_PASS = process.env.MONGO_PASS
-const databaseName = 'nasa'
-const MONGO_URL = `mongodb+srv://nasa-api:${MONGO_PASS}@nasacluster.elia6pz.mongodb.net/${databaseName}?retryWrites=true&w=majority`
+const MONGO_URL = process.env.MONGO_URL
 
 mongoose.connection.on('open',()=>{ // mongoose.connection is an event emmiter
     console.log('MongoDB connection ready!');
